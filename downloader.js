752626b53240b4a1da01ff6a3312aa395ed41f68
notsapinho <52896767+notsapinho@users.noreply.github.com>
2020-09-03 22:35:17 -0300

    Highlights support

--- downloader.js
+++ downloader.js
@@ -1,28 +1,33 @@
 const fetch = require("node-fetch");
+require("dotenv/config");
 
-async function download_posts(posts_src) {
-    const posts = [];
+async function download(srcs) {
+    const res = [];
 
-    for (let i = 0; i < posts_src.length; i++) {
-        posts.push(await get_data_from(posts_src[i]));
+    for (let i = 0; i < srcs.length; i++) {
+        if (srcs[i].srcs) {
+            for (let j = 0; j < srcs[i].srcs.length; j++) {
+                res.push(await get_data_from(srcs[i].srcs[j], srcs[i].title));
+            }
+        } else res.push(await get_data_from(srcs[i]));
     }
 
-    return posts;
+    return res;
 }
 
-async function get_data_from(url) {
+async function get_data_from(url, title) {
     const response = await fetch(url);
 
     const data = await response.arrayBuffer();
     const extension = "." + response.headers.get("content-type").split("/")[1];
 
-    return { data: data, extension: extension };
+    return { data: data, extension: extension, title };
 }
 
 async function get_posts_from_instagram(page, timeout = 3000) {
     const posts = await page.evaluate(get_posts_hrefs, timeout);
 
-    return await download_posts(await get_srcs_from_posts(posts));
+    return await download(await get_srcs_from_posts(posts));
 }
 
 async function get_srcs_from_posts(posts) {
@@ -71,4 +76,49 @@ async function get_posts_hrefs(timeout) {
     return urls;
 }
 
-module.exports = get_posts_from_instagram;
+async function get_highlights_srcs(stories) {
+    const res = await (
+        await fetch(
+            `https://www.instagram.com/graphql/query/?query_hash=90709b530ea0969f002c86a89b4f2b8d&variables={"reel_ids":[],"tag_names":[],"location_ids":[],"highlight_reel_ids":${JSON.stringify(
+                stories
+            )},"precomposed_overlay":false,"show_story_viewer_list":false,"story_viewer_fetch_count":0,"story_viewer_cursor":"","stories_video_dash_manifest":false}`,
+            {
+                method: "GET",
+                headers: {
+                    "x-ig-capabilities": "3w==",
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0",
+                    Host: "i.instagram.com",
+                    Cookie: `sessionid=${process.env.SESSION_ID}; ds_user_id=${process.env.USER_ID};`
+                }
+            }
+        )
+    ).json();
+
+    return res.data.reels_media.map((x) => ({ title: x.id, srcs: x.items.map((y) => (y.is_video ? y.video_resources[0].src : y.display_url)).flat() }));
+}
+
+async function get_highlights(id) {
+    const res = await (
+        await fetch(
+            `https://www.instagram.com/graphql/query/?query_hash=d4d88dc1500312af6f937f7b804c68c3&variables={"user_id":"${id}","include_chaining":false,"include_reel":false,"include_suggested_users":false,"include_logged_out_extras":false,"include_highlight_reels":true,"include_related_profiles":false}`,
+            {
+                method: "GET",
+                headers: {
+                    "x-ig-capabilities": "3w==",
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0",
+                    Host: "i.instagram.com",
+                    Cookie: `sessionid=${process.env.SESSION_ID}; ds_user_id=${process.env.USER_ID};`
+                }
+            }
+        )
+    ).json();
+
+    return res.data.user.edge_highlight_reels.edges.map((x) => x.node.id);
+}
+
+module.exports = {
+    get_posts_from_instagram,
+    get_highlights,
+    get_highlights_srcs,
+    download
+};
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const downloader = require("./downloader");
+const { get_posts_from_instagram, get_highlights, get_highlights_srcs, download } = require("./downloader");
 const fetch = require("node-fetch");
 const logger = require("ora");
 const fs = require("fs");
@@ -11,18 +11,19 @@ const spinner = {
     frames: ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"]
 };
 
-async function download_posts_from_instagram(username, dir = null) {
+async function download_posts_from_instagram(username, hl = false, dir = null) {
     if (!username) return process.exit();
 
-    const download_path = dir || path.join("downloads/" + username);
+    const download_path = dir || path.join("downloads/", username);
+    const highlight_path = dir || path.join("downloads/", username, "/stories");
     const url = "https://www.instagram.com/" + username;
 
     const res = await (await fetch(url + "?__a=1")).json();
 
-    if (res === {}) {
+    if (res === {} || !res) {
         console.log("[INVALID ACCOUNT]");
         process.exit();
-    } else if (res.graphql.user.is_private) {
+    } else if (res && res.graphql.user.is_private) {
         console.log("[PRIVATE ACCOUNT]");
         process.exit();
     }
@@ -57,7 +58,7 @@ async function download_posts_from_instagram(username, dir = null) {
         spinner
     }).start();
 
-    const posts = await downloader(page, 3000);
+    const posts = await get_posts_from_instagram(page, 3000);
 
     postsLog.succeed("[POSTS DOWNLOADED]");
 
@@ -73,11 +74,52 @@ async function download_posts_from_instagram(username, dir = null) {
         fs.writeFileSync(filename, Buffer.from(posts[i].data));
     }
 
-    await browser.close();
     savingLog.succeed("[VIDEOS SAVED]");
+
+    await browser.close();
+
+    if (hl) {
+        if (!fs.existsSync(highlight_path)) fs.mkdirSync(highlight_path);
+
+        const getHighlights = logger({
+            text: "[GETTING HIGHLIGHTS]",
+            spinner
+        }).start();
+
+        const highlights = await get_highlights_srcs(await get_highlights(res.logging_page_id.replace("profilePage_", "")));
+
+        getHighlights.succeed("[GOT HIGHLIGHTS]");
+
+        const downloadHighlights = logger({
+            text: "[DOWNLOADING HIGHLIGHTS]",
+            spinner
+        }).start();
+
+        const downloadedhighlights = await download(highlights);
+
+        downloadHighlights.succeed("[DOWNLOADED HIGHLIGHTS]");
+
+        const saveHighlights = logger({
+            text: "[SAVING HIGHLIGHTS]",
+            spinner
+        }).start();
+
+        const qtds = [...new Set(downloadedhighlights.map((x) => x.title))].reduce((o, key) => ({ ...o, [key]: 0 }), {});
+
+        for (let i = 0; i < downloadedhighlights.length; i++) {
+            if (!fs.existsSync(path.join(highlight_path, downloadedhighlights[i].title))) fs.mkdirSync(path.join(highlight_path, downloadedhighlights[i].title));
+
+            const filename = path.join(highlight_path, path.join(downloadedhighlights[i].title, String(qtds[downloadedhighlights[i].title]++) + downloadedhighlights[i].extension));
+            fs.writeFileSync(filename, Buffer.from(downloadedhighlights[i].data));
+        }
+
+        saveHighlights.succeed("[SAVED HIGHLIGHTS]");
+    }
 
     process.exit();
 }
+
+console.clear();
 
 const input = readline.createInterface({
     input: process.stdin,
